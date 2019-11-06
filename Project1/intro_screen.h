148ca6d121420050c1b373cefea87a0558bcb9cf
Jorge Allar <jorgex.allar.guzaman@intel.com>
2019-11-06 17:21:27 -0600

    into screen added

---
+++ Project1/intro_screen.h
@@ -0,0 +1,32 @@
+#pragma once
+#using <mscorlib.dll>
+#using <System.dll>
+#using <System.Drawing.dll>
+#using <System.Windows.Forms.dll>
+
+using namespace System;
+using namespace System::Drawing;
+using namespace System::Windows::Forms;
+
+ref class intro_screen:
+	public Form
+{
+public:
+	intro_screen();
+	int* getResponsiveLocation(int x_size, int y_size);
+
+	unsigned int MAX_HEIGHT_WINDOW = 780;
+	unsigned int MAX_WIDTH_WINDOW = 640;
+private:
+	PictureBox^ logo;
+	Label^		u_label;
+	Label^		p_label;
+	TextBox^	user;
+	TextBox^	password;
+	CheckBox^	unlock_password;
+	Button^		login_btn;
+
+	void showPassword(Object^ Sender, EventArgs^ e);
+	void logUser(Object^ Sender, EventArgs^ e);
+};
+
