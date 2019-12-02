23a12ab2c76833b6c1715f9c1099e287354e9f3b
Jorge Allar <jorgex.allar.guzaman@intel.com>
2019-12-02 17:17:29 -0600

    Declaration of FileHandler class (need implementation), please review softFlow

--- Project3/Project3/FileHandler.cpp
+++ Project3/Project3/FileHandler.cpp
@@ -10,3 +10,34 @@ FileHandler::FileHandler()
 FileHandler::~FileHandler()
 {
 }
+
+//Not sure what to do with this function, don't delete could be useful in the future
+bool FileHandler::rootDirectory()
+{
+	return true;
+}
+
+//Check all install folders previously created by the WinRar install tool
+bool FileHandler::checkInstallIntegrity()
+{
+	return true;
+}
+
+//During first install this function will help to set the admin user and password as well, also implemented when creating users
+int FileHandler::writeUsrData(int dDataType, string dDataContent)
+{
+	return 0;
+}
+
+//Conversor from string to binary to encode data
+int FileHandler::conStrToBin(string pStrData)
+{
+	return 0;
+}
+
+//Conversor from binary to String for decode data
+string FileHandler::conBinToStr(int pBinData)
+{
+	string lol;
+	return lol;
+}
--- Project3/Project3/FileHandler.h
+++ Project3/Project3/FileHandler.h
@@ -1,7 +1,15 @@
 #pragma once
+
+#include <iostream>
+#include <stdlib.h>
+
 #include "logger.h"
+#include "boost\filesystem.hpp"
 #include "boost\filesystem\path.hpp"
 #include "boost\filesystem\operations.hpp"
+
+using namespace std;
+using namespace boost::filesystem;
 
 class FileHandler :
 	public logger
@@ -9,5 +17,19 @@ class FileHandler :
 public:
 	FileHandler();
 	~FileHandler();
+
+	bool rootDirectory();
+	bool checkInstallIntegrity();
+	int  writeUsrData(int dDataType, string dDataContent);
+
+private:
+	int conStrToBin(string pStrData);
+	string conBinToStr(int pBinData);
+
+	string iMainFolder = "\\pdv\\";
+	string iBinFolder = "\\Bin\\";
+	string iFilesFolder = "\\Files\\";
+	string iRecoveryFolder = "\\Recovery\\";
+	string iDllFolder = "\\dll\\";
 };
 
